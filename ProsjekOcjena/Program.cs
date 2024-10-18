aeb773b3c4030bd3feeebf832895ae54a88ce451
midomlijavern <midomlija@vernnet.hr>
2024-10-18 18:29:45 +0200

    tipovi i varijable

---
+++ ProsjekOcjena/Program.cs
@@ -0,0 +1,18 @@
+﻿try
+{
+	Console.Write("Ocjena C: ");
+	string unos = Console.ReadLine();
+	int ocjenaC = int.Parse(unos);
+	Console.Write("Ocjena C++: ");
+	unos = Console.ReadLine();
+	int ocjenaCPlus = int.Parse(unos);
+	Console.Write("Ocjena C#: ");
+	unos = Console.ReadLine();
+	int ocjenaCSharp = int.Parse(unos);
+	double prosjek = (ocjenaC + ocjenaCPlus + ocjenaCSharp) / 3.0;
+	Console.WriteLine($"Prosjek ocjena je {prosjek:N2}");
+}
+catch (Exception ex)
+{
+	Console.WriteLine($"Dogodila se pogreška. Tekst: {ex.Message}");
+}
