41c4f2ff196af618d2aefa5183b2d588124aa975
midomlijavern <midomlija@vernnet.hr>
2024-10-25 18:23:53 +0200

    Naredbe odluke

---
+++ DaniMjeseca/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Write("Unesite redni broj mjeseca (1-12): ");
+string unos = Console.ReadLine();
+bool ok = int.TryParse(unos, out int redniBrojMjeseca);
+if(!ok)
+{
+    Console.WriteLine("Pogrešan format broja");
+    return;
+}
+if(redniBrojMjeseca < 1 || redniBrojMjeseca > 12)
+{
+    Console.WriteLine("Broj je izvan raspona");
+    return;
+}
+int brojDana = 0;
+switch(redniBrojMjeseca)
+{
+    case 2:
+        if (DateTime.IsLeapYear(DateTime.Now.Year))
+            brojDana = 29;
+        else
+            brojDana = 28;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        brojDana = 30;
+        break;
+    default:
+        brojDana = 31;
+        break;
+}
+string dani;
+if (brojDana == 31)
+    dani = "dan";
+else
+    dani = "dana";
+Console.WriteLine($"{redniBrojMjeseca}. mjesec ima {brojDana} {dani}");
+
---
+++ DatumiRodenja/Program.cs
@@ -0,0 +1,52 @@
+﻿Console.Write("Datum rođenja osobe 1: ");
+string unos = Console.ReadLine();
+bool ok = DateTime.TryParse(unos, out DateTime datum1);
+if(!ok)
+{
+    Console.WriteLine("Pogrešan format datuma");
+    return;
+}
+Console.Write("Datum rođenja osobe 2: ");
+unos = Console.ReadLine();
+ok = DateTime.TryParse(unos, out DateTime datum2);
+if (!ok)
+{
+    Console.WriteLine("Pogrešan format datuma");
+    return;
+}
+if(datum1 < datum2)
+{
+    Console.WriteLine($"Osoba 1 je starija od osobe 2 za {(datum2 - datum1).TotalDays} dana");
+}
+else if(datum1 > datum2)
+{
+    Console.WriteLine($"Osoba 2 je starija od osobe 1 za {(datum1 - datum2).TotalDays} dana");
+}
+else
+{
+    Console.WriteLine("Osobe su rođene istog datuma");
+}
+
+if(datum1.Month < datum2.Month)
+{
+    Console.WriteLine("Osoba 1 slavi rođendan prije osobe 2");
+}
+else if(datum1.Month > datum2.Month)
+{
+    Console.WriteLine("Osoba 2 slavi rođendan prije osobe 1");
+}
+else if(datum1.Day < datum2.Day)
+{
+    Console.WriteLine("Osoba 1 slavi rođendan prije osobe 2");
+}
+else if(datum1.Day > datum2.Day)
+{
+    Console.WriteLine("Osoba 2 slavi rođendan prije osobe 1");
+}
+else
+{
+    Console.WriteLine("Osobe slave rođendan isti dan");
+}
+
+
+
